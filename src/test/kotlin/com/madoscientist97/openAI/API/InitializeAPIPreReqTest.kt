ad1d746fd328da3b26378f24f8b17d25c4b09fec
MadoScientist97 <sahilthehi3r7@gmail.com>
2024-05-09 01:25:41 +0530

    restored incomplete files

--- src/test/kotlin/com/madoscientist97/openAI/API/InitializeAPIPreReqTest.kt
+++ src/test/kotlin/com/madoscientist97/openAI/API/InitializeAPIPreReqTest.kt
@@ -1,5 +1,20 @@
 package com.madoscientist97.openAI.API
 
 import junit.framework.TestCase
+import org.junit.Test
+import kotlin.test.assertEquals
 
-class InitializeAPIPreReqTest : TestCase()
+class InitializeAPIPreReqTest {
+
+    @Test
+    fun checkCorrectKeyRead() {
+        val initialized = InitializeAPIPreReq();
+        assertEquals("test_key", initialized.getAPIKey());
+    }
+
+    @Test(expected = Exception::class)
+    fun wrongPropFilePassed() {
+        val initialized =InitializeAPIPreReq("wrong_file");
+    }
+
+}
